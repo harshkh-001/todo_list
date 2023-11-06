72b70ac6f90723fee920c56a7208cd073c83fc0c
harshkh-001 <harshkhandelwal1245@gmail.com>
2023-11-06 18:50:08 +0530

    todo list

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 from todolist import views
+from .views import adding
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('adding' , views.adding , name = "adding"),
     path('return_list' , views.return_list , name = "return_list"),
-    path('display' , views.returning , name = "returning")
+    path('display' , views.returning , name = "returning"),
+    path('signup' , views.sign , name = "signup"),
+    path('sign_up' , views.sign_up , name = "sign_up")
+    # path('/redirect/' , adding)
 ]
--- todolist/views.py
+++ todolist/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render , HttpResponse 
+from django.shortcuts import render , HttpResponse , redirect
 import pymongo as py
 from django.template import loader
 from django.http import HttpResponse
@@ -9,19 +9,42 @@ def home(request):
     return render(request , "html_home.html")
 # Create your views here.
 
+def sign(request):
+    return render(request , "signup.html")
+
+def sign_up(request):
+    name = request.GET.get('s_name', "defalt")
+    pas = request.GET.get('s_password', "defalt")
+    if (name == "" or pas == ""):
+        return HttpResponse("name or what to do cannot be empty")
+    client = py.MongoClient("mongodb://localhost:27017")
+    db = client["todoproject"]
+    collection = db["password_manager"]
+    collection.insert_one({"name":name , "password":pas}) 
+    return render(request , "html_home.html")
+    
 def adding(request):
     todo = request.GET.get('todo', "defalt")
     name = request.GET.get('name', "defalt")
+    pas = request.GET.get('password', "defalt")
     if (name == "" or todo == ""):
         return HttpResponse("name or what to do cannot be empty")
     client = py.MongoClient("mongodb://localhost:27017")
     db = client["todoproject"]
-    collection = db["todolist"]
-    idd = collection.count_documents({})
-    collection.insert_one({"_id": idd+1 , "Name" : name , "todo" : todo})
-    print(request.GET.get('todo', "defalt"))
-    return render(request , "html_home.html")
-
+    collect = db["password_manager"]
+    all_data = collect.find({"name":name} , {"_id" : 0 , "password" : 1})
+    for list1 in all_data:
+        for password in list1.values():
+            if(password == pas):
+                client = py.MongoClient("mongodb://localhost:27017")
+                db = client["todoproject"]
+                collection = db["todolist"]
+                idd = collection.count_documents({})
+                collection.insert_one({"_id": idd+1 , "Name" : name ,"password":pas,"todo" : todo})
+                print(request.GET.get('todo', "defalt"))
+                response = redirect(".")
+                return response
+    return HttpResponse("Incorrect password or name kindly check")
 
 def return_list(request):
     return render(request , "return_list.html")
@@ -32,7 +55,9 @@ def returning(request):
     db = client["todoproject"]
     collection = db["todolist"]
     name = request.GET.get("name" , "defalt")
-    
+    pas = request.GET.get('password', "defalt")
+    if (name == "" or pas == ""):
+        return HttpResponse("name or what to do cannot be empty")
     all_data = collection.find({"Name": name} , {"_id": 0  ,"todo": 1})
     
     text = []
@@ -55,7 +80,15 @@ def returning(request):
         "variable" : z,
         "members" : text
     }
-      
-    return render(request ,"return_list.html", context)
-         
+    
+    client = py.MongoClient("mongodb://localhost:27017")
+    db = client["todoproject"]
+    collect = db["password_manager"]
+    all_data = collect.find({} , {"_id" : 0 , "password" : 1})
+    for list1 in all_data:
+        for password in list1.values():
+            if(password == pas): 
+    
+                return render(request ,"return_list.html", context)
+    return HttpResponse("No data Exist with given password")   
    
