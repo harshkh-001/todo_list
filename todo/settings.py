725fda8e6e41ff3d7a829d5bc67c52fb1df9ac56
harshkh-001 <147712949+harshkh-001@users.noreply.github.com>
2024-06-13 13:11:07 +0530

    Update settings.py

--- todo/settings.py
+++ todo/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 ]
 
 MIDDLEWARE = [
